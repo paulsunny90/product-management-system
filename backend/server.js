e31739b443102e18ea473535b544217b7e33748d
paulsunny90 <paulsunny87267@gmail.com>
2026-01-31 13:28:03 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+
+
+
+const app = express()
+
+const PORT = process.env.PORT||5000
+app.listen(PORT,()=>{
+ console.log(`Server running on http://localhost:${PORT}`)
+})
+
+
+
+
